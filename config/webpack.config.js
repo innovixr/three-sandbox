124d722d73dff9f39a2bbc1f00611187d43fe50f
eviltik <eviltik@gmail.com>
2022-04-24 17:04:15 +0200

    cosmetic

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -2,22 +2,15 @@
 
 // eslint-disable-next-line no-unused-vars
 module.exports = env => {
-
 	return {
-
 		mode: 'production',
-
 		entry: {
 			'../build/three-sandbox': './src/three-sandbox.js',
 		},
-
 		output: {
 			filename: '[name].js',
 			library: 'ThreeSandbox',
 			libraryTarget: 'umd'
 		}
-
-
 	};
-
 };
