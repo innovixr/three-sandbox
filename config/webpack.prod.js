57849f2afe2031905ca25b3835f96cc9ba1789d9
eviltik <eviltik@gmail.com>
2022-04-26 16:27:19 +0200

    refactor

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,6 +1,6 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const networkInterfaces = require('os').networkInterfaces;
+const path = require( 'path' );
+const HtmlWebpackPlugin = require( 'html-webpack-plugin' );
+const networkInterfaces = require( 'os' ).networkInterfaces;
 
 const MODULE_NAME = 'three-sandbox';
 const PUBLIC_PATH = 'public';
@@ -8,28 +8,38 @@ const TEMPLATE_PATH = `../${PUBLIC_PATH}/html`;
 
 // data in format [ JS file name => demo title in public pages ]
 let pages = [
-	['kit_basic_setup', 'Basic setup'],
-	['exp_basic_setup', 'Basic setup'],
-	['datguixr_keyboard', 'Keyboard'],
-	['exp_keyboard_three', 'Threejs Keyboard'],
-	['three_webgl', 'Three WebGL texture'],
-	['three_htmlmesh', 'Three HTML Mesh'],
+	[ 'kit_basic_setup', 'Basic setup' ],
+	[ 'exp_basic_setup', 'Basic setup' ],
+	[ 'exp_keyboard_three', 'Keyboard (+pixi)' ],
+	[ 'three_webgl', 'THREE WebGL test' ],
+	[ 'three_htmlmesh', 'THREE.HTMLMesh test' ],
+	[ 'datguixr_keyboard', 'Keyboard' ],
+	[ 'pixi_layout', 'Layout' ],
 ];
 
-function getIpAddress(interfaceRegexp) {
-	console.assert(interfaceRegexp instanceof RegExp);
+const keys = [
+	'exp',
+	'kit',
+	'datguixr',
+	'three',
+	'pixi'
+];
+
+
+function getIpAddress( interfaceRegexp ) {
+	console.assert( interfaceRegexp instanceof RegExp );
 
 	const nets = networkInterfaces();
-	const keys = Object.keys(nets);
+	const keys = Object.keys( nets );
 	let ip, name, net;
 
-	for (name of keys) {
-		for (net of nets[name]) {
+	for ( name of keys ) {
+		for ( net of nets[name] ) {
 			// Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
 			if (
 				net.family === 'IPv4'
 				&& !net.internal
-				&& name.match(interfaceRegexp)
+				&& name.match( interfaceRegexp )
 			) {
 				ip = net.address;
 			}
@@ -38,45 +48,43 @@ function getIpAddress(interfaceRegexp) {
 
 	}
 
-	if (!ip) {
-		throw new Error('Error: could not fetch private IP address');
+	if ( !ip ) {
+		throw new Error( 'Error: could not fetch private IP address' );
 	}
 
 	return ip;
 }
 
 // create one config for each of the data set above
-let plugins = pages.map((page) => {
-	return new HtmlWebpackPlugin({
+let plugins = pages.map( ( page ) => {
+	return new HtmlWebpackPlugin( {
 		title: page[0],
 		filename: page[0] + '.html',
-		template: path.resolve(__dirname, `${TEMPLATE_PATH}/template.html`),
-		chunks: [page[0], MODULE_NAME],
+		template: path.resolve( __dirname, `${TEMPLATE_PATH}/template.html` ),
+		chunks: [ page[0], MODULE_NAME ],
 		inject: true
-	});
-});
+	} );
+} );
 
-const allPages = {
-	kit: pages.filter(x => x[0].indexOf('kit_') === 0).reduce(pageReducer, ''),
-	experience: pages.filter(x => x[0].indexOf('exp_') === 0).reduce(pageReducer, ''),
-	datguixr: pages.filter(x => x[0].indexOf('datguixr_') === 0).reduce(pageReducer, ''),
-	three: pages.filter(x => x[0].indexOf('three_') === 0).reduce(pageReducer, ''),
-};
+const allPages = {};
 
-function pageReducer(accu, page) {
+for ( let key of keys )
+	allPages[ key ] = pages.filter( x => x[ 0 ].indexOf( `${key}_` ) === 0 ).reduce( pageReducer, '' );
+
+function pageReducer( accu, page ) {
 	return accu + `<li title="${page[0]}">${page[1]}</li>`;
 }
 
-const indexConfig = new HtmlWebpackPlugin({
+const indexConfig = new HtmlWebpackPlugin( {
 	pages: allPages,
 	environment: {
 		production: false
 	},
 	filename: 'index.html',
-	template: path.resolve(__dirname, `${TEMPLATE_PATH}/index.html`),
+	template: path.resolve( __dirname, `${TEMPLATE_PATH}/index.html` ),
 	inject: false
-});
-plugins.push(indexConfig);
+} );
+plugins.push( indexConfig );
 
 module.exports = env => {
 
@@ -84,24 +92,24 @@ module.exports = env => {
 	let devtool = 'eval-source-map';
 
 	// Prod environment
-	if (env.NODE_ENV === 'prod') {
+	if ( env.NODE_ENV === 'prod' ) {
 		devtool = false;
 		mode = 'production';
 	}
 
 	const entry = {};
 	entry[`../dist/${MODULE_NAME}`] = `./src/${MODULE_NAME}.js`;
-	for (let page of pages) {
+	for ( let page of pages ) {
 		entry[page[0]] = `./${PUBLIC_PATH}/${page[0]}.js`;
 	}
 
 	const alias = {};
 	// add an alias of for itself in order to make examples more real-case
 	// scenario and avoiding ../src/..;
-	alias[`${MODULE_NAME}/${PUBLIC_PATH}`] = path.resolve(__dirname, `../${PUBLIC_PATH}/`);
-	alias[`${MODULE_NAME}`] = path.resolve(__dirname, `../src/${MODULE_NAME}.js`);
-	alias['DatGuiXR'] = path.resolve(__dirname, '../src/DatGuiXR/DatGuiXR.js');
-	alias['three-kit'] = path.resolve(__dirname, '../src/KIT/KIT.js');
+	alias[`${MODULE_NAME}/${PUBLIC_PATH}`] = path.resolve( __dirname, `../${PUBLIC_PATH}/` );
+	alias[`${MODULE_NAME}`] = path.resolve( __dirname, `../src/${MODULE_NAME}.js` );
+	alias['DatGuiXR'] = path.resolve( __dirname, '../src/DatGuiXR/DatGuiXR.js' );
+	alias['three-kit'] = path.resolve( __dirname, '../src/KIT/KIT.js' );
 
 	//alias[ `/assets/`] = path.resolve( __dirname, `../${PUBLIC_PATH}/assets/` );
 
@@ -112,17 +120,17 @@ module.exports = env => {
 		plugins,
 		devServer: {
 			static: {
-				directory: path.join(__dirname, `../${PUBLIC_PATH}/assets/`),
+				directory: path.join( __dirname, `../${PUBLIC_PATH}/assets/` ),
 				publicPath: '/assets',
 			},
 			server: env.NODE_SSL ? 'https' : 'http',
 			port: env.NODE_SSL ? '8443' : '8080',
-			host: env.NODE_SSL ? getIpAddress(/wi-fi|eth0|Ethernet/i) : 'localhost'
+			host: env.NODE_SSL ? getIpAddress( /wi-fi|eth0|Ethernet/i ) : 'localhost'
 		},
 
 		output: {
 			filename: '[name].js',
-			path: path.resolve(__dirname, '../dist')
+			path: path.resolve( __dirname, '../dist' )
 		},
 
 		resolve: {
@@ -133,7 +141,7 @@ module.exports = env => {
 			rules: [
 				{
 					test: /\.(png|svg|jpg|gif)$/,
-					use: ['file-loader'],
+					use: [ 'file-loader' ],
 				}
 			],
 		}
---
+++ public/pixi_layout.js
@@ -0,0 +1,226 @@
+//import * as THREE from 'three';
+//import App from './App.js';
+//import KIT from 'three-kit';
+
+import * as PIXI from 'pixi.js-legacy';
+import '../src/DatGuiXR/pixi/center.js';
+
+window.addEventListener( 'load', init );
+
+console.clear();
+
+const lines = [
+	[
+		{ width: 0.1, chars: 'A' },
+		{ width: 0.1, chars: 'Z' },
+		{ width: 0.1, chars: 'E' },
+		{ width: 0.1, chars: 'R' },
+		{ width: 0.1, chars: 'T' },
+		{ width: 0.1, chars: 'Y' },
+		{ width: 0.1, chars: 'U' },
+		{ width: 0.1, chars: 'I' },
+		{ width: 0.1, chars: 'O' },
+		{ width: 0.1, chars: 'P' }
+	],
+	[
+		{ width: 0.1, chars: 'Q' },
+		{ width: 0.1, chars: 'S' },
+		{ width: 0.1, chars: 'D' },
+		{ width: 0.1, chars: 'F' },
+		{ width: 0.1, chars: 'G' },
+		{ width: 0.1, chars: 'H' },
+		{ width: 0.1, chars: 'J' },
+		{ width: 0.1, chars: 'K' },
+		{ width: 0.1, chars: 'L' },
+		{ width: 0.1, chars: 'M' }
+	],
+	[
+		{ width: 0.2, chars: 'up' },
+		{ width: 0.1, chars: 'W' },
+		{ width: 0.1, chars: 'X' },
+		{ width: 0.1, chars: 'C' },
+		{ width: 0.1, chars: 'V' },
+		{ width: 0.1, chars: 'B' },
+		{ width: 0.1, chars: 'N' },
+		{ width: 0.2, chars: 'back' }
+	],
+	[
+		{ width: 0.2, chars: '.?12' },
+		{ width: 0.1, chars: ',' },
+		{ width: 0.4, chars: 'space' },
+		{ width: 0.1, chars: '.' },
+		{ width: 0.2, chars: 'enter' }
+	]
+];
+
+class Keyboard {
+
+	constructor( opts ) {
+		opts = opts || {};
+		this.opts = opts;
+		this.canvasWidth = opts.width || 1024;
+		PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
+
+		this.backdropPadding = this.getCanvasPercentWidth( 1.3 );
+		this.backdropRadius = this.getCanvasPercentWidth( 2 );
+		this.backdropColor = 0x030303;
+
+		this.buttonMargin = this.getCanvasPercentWidth( 1 );
+		this.buttonBaseWidth = this.getButtonBaseWidth();
+		this.buttonRadius = this.getCanvasPercentWidth( 1 );
+		this.buttonBaseHeight = this.buttonBaseWidth;
+		this.buttonColor = 0x444444;
+		this.buttonColorText = 0xFFFFFF;
+		this.buttonFont = { fontFamily: 'Verdana', fontSize: this.canvasWidth / 30 };
+		this.canvasHeight = this.getCanvasHeight();
+
+		this.createCanvas( this.canvasWidth, this.canvasHeight, opts.showCanvas );
+		this.createPixiApp();
+		this.createBackdrop( this.canvasWidth, this.canvasHeight, this.backdropRadius, this.backdropColor );
+		this.createButtons();
+
+		//console.log( `base width = ${this.buttonBaseWidth}, height = ${this.buttonBaseHeight}, canvas height = ${this.canvasHeight}` );
+		document.body.style.backgroundColor = '#505050';
+	}
+
+	getButtonBaseWidth() {
+		const maxButtons = this.getMaxButtons();
+		const buttonMargins = this.backdropPadding + ( this.buttonMargin * ( maxButtons - 1 ) ) + this.backdropPadding;
+		const freeSpace = this.canvasWidth - buttonMargins;
+		const buttonWidth = freeSpace / maxButtons;
+		return buttonWidth;
+	}
+
+	getCanvasHeight() {
+		const totalPaddingCanvas = ( this.backdropPadding * 2 );
+		const totalPaddingLines = this.buttonMargin * ( lines.length - 1 );
+		const buttonsHeight = this.buttonBaseHeight * lines.length;
+		const canvasHeight = totalPaddingCanvas + totalPaddingLines + buttonsHeight;
+		return canvasHeight;
+	}
+
+	getCanvasPercentWidth( percent ) {
+		return ( percent * this.canvasWidth / 100 );
+	}
+
+
+	createCanvas( width, height, show ) {
+		const canvasEl = document.createElement( 'canvas' );
+		canvasEl.width = width;
+		canvasEl.height = height;
+		let style = '';
+		style += `position:absolute;width:${width}px;height:${height}px;`;
+		style += 'margin:auto; top:0; left:0; right:0; bottom:0;';
+		style += 'background-color: transparent; opacity: 0.999;';
+		//style += 'zoom: 0.5;';
+		if ( !show )
+			style += 'visibility:hidden; z-index: 1000;';
+		canvasEl.style = style;
+
+		document.body.appendChild( canvasEl );
+		this.canvasEl = canvasEl;
+		return canvasEl;
+	}
+
+	createPixiApp() {
+		this.pixiApp = new PIXI.Application( {
+			view: this.canvasEl,
+			backgroundAlpha: 0,
+			backgroundColor: 0x000000,
+			width: this.canvasWidth,
+			height: this.canvasHeight,
+			//legacy: true,
+			//clearBeforeRender: true,
+			//forceCanvas:true,
+			//interactive:true,
+			//autoResize: true,
+			//powerPreference: 'high-performance'
+		} );
+	}
+
+	createBackdrop( width, height, radius, fillColor ) {
+		this.backdrop = this.createPanel( width, height, radius, fillColor );
+		this.pixiApp.stage.addChild( this.backdrop );
+	}
+
+	createButtons() {
+		let firstLine = true;
+		let firstKey = true;
+		let accumulateX = 0;
+		let accumulateY = 0;
+		let button;
+		let width = this.buttonBaseWidth;
+		let height = this.buttonBaseHeight;
+		let radius = this.buttonRadius;
+
+		lines.forEach( ( line ) => {
+			firstKey = true;
+			line.forEach( ( key ) => {
+				if ( firstLine ) accumulateY = this.backdropPadding;
+				if ( firstKey ) accumulateX = this.backdropPadding;
+
+				// compute button width
+				key.width = key.width * 10;
+				width = this.buttonBaseWidth * ( key.width );
+				width += this.buttonMargin * ( key.width - 1 );
+
+				// create button
+				button = this.createButton( width, height, radius, this.buttonColor, key.chars );
+
+				// position the button
+				button.position.x = accumulateX;
+				button.position.y = accumulateY;
+
+				// add the button to his container
+				this.backdrop.addChild( button );
+
+				firstKey = false;
+				accumulateX += width + this.buttonMargin;
+			} );
+			firstLine = false;
+			accumulateY += height + this.buttonMargin;
+		} );
+	}
+
+	createPanel( width, height, radius, fillColor ) {
+		const panel = new PIXI.Graphics();
+		//panel.lineStyle( { alignment: 0, width: 5, color: 0x2222FF, alpha: 0.2 } );
+		panel.beginFill( fillColor, 0.8 );
+		panel.drawRoundedRect( 0, 0, width, height, radius );
+		panel.endFill();
+		return panel;
+	}
+
+
+	createButton( width, height, radius, fillColor, str ) {
+		const button = this.createPanel( width, height, radius, fillColor );
+		const text = new PIXI.Text( str, this.buttonFont );
+		text.style.fill = this.buttonColorText;
+		button.addChild( text );
+		text.anchor.set( 0.5 );
+		text.centerXY();
+		return button;
+	}
+
+	getMaxButtons() {
+		let max = 0;
+		lines.forEach( ( line ) => {
+			let tmp = 0;
+			line.forEach( key => tmp += key.width * 10 );
+			if ( tmp > max ) max = tmp;
+		} );
+		return max;
+	}
+
+	round2( v ) {
+		return Math.round( v * 100 ) / 100;
+	}
+
+}
+
+function init() {
+	new Keyboard( {
+		showCanvas: true
+	} );
+}
+
