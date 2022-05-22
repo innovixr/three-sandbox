9e23a07f316cd73248d68bf8ccd4f25f096cfa8d
eviltik <eviltik@gmail.com>
2022-05-22 07:57:55 +0200

    RTMP_URL

--- public/three_obs.js
+++ public/three_obs.js
@@ -2,6 +2,8 @@ import * as THREE from 'three';
 import flvjs from 'flv.js';
 import App from './App.js';
 
+const RTMP_URL = 'wss://rtmp.innovacode.com:1933/livetest/test.flv';
+
 window.addEventListener( 'load', () => {
 	new Experience();
 } );
@@ -28,17 +30,20 @@ class Experience {
 		this.videoElement = document.createElement( 'video' );
 		this.videoElement.setAttribute( 'class', 'video' );
 		//this.videoElement.volume = 0;
-		this.videoElement.muted = true;
+		//this.videoElement.muted = true;
 		this.videoElement.autoplay = true;
 
 		document.body.appendChild( this.videoElement );
 
 		const flvPlayer = flvjs.createPlayer( {
 			type: 'flv',
-			isLive:true,
-			url: 'https://192.168.1.100:8001/livetest/test.flv'
+			isLive: true,
+
+			url: RTMP_URL
 		}, {
 			enableWorker: false,
+			//enableStashBuffer: false,
+			//stashInitialSize: 128,
 			lazyLoadMaxDuration: 3 * 60,
 			seekType: 'range',
 		} );
@@ -51,14 +56,14 @@ class Experience {
 		this.videoTexture = new THREE.VideoTexture( this.videoElement );
 		this.videoTexture.encoding = THREE.sRGBEncoding;
 		this.videoTexture.needsUpdate = true;
-		//videoTexture.minFilter = THREE.LinearFilter;
-		//videoTexture.magFilter = THREE.LinearFilter;
-		const width = 4;
+		this.videoTexture.minFilter = THREE.LinearFilter;
+		this.videoTexture.magFilter = THREE.LinearFilter;
+		const width = 2;
 		const height = width * 0.5625;
 		const geometry = new THREE.PlaneGeometry( width, height );
 		const material = new THREE.MeshBasicMaterial( { map: this.videoTexture } );
 		const mesh = new THREE.Mesh( geometry, material );
-		mesh.position.set( 0, 2.4 - ( height / 2 ), -4.9 );
+		mesh.position.set( 0, 2 - ( height / 2 ), -1.5 );
 
 		//material.map.needsUpdate = true;
 		//material.needsUpdate = true;
