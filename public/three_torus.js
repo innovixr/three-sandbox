b3b7bfbf978b7fe24b100584a7d7410da55284f0
eviltik <eviltik@gmail.com>
2022-05-26 10:25:49 +0200

    improve torus

--- public/three_torus.js
+++ public/three_torus.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import App from './App.js';
+import { GUI } from 'lil-gui';
 
 window.addEventListener( 'load', () => {
 	new Experience();
@@ -7,28 +8,44 @@ window.addEventListener( 'load', () => {
 
 
 class Experience {
+
 	constructor() {
-		this.app = new App( this.loop.bind( this ) );
+		this.app = new App( {
+			//lights: false,
+			lightsIntensity:0.3
+		} );
+		this.app.addTicker( this.loop.bind( this ) );
 		this.renderer = this.app.renderer;
 		this.camera = this.app.camera;
 		this.scene = this.app.scene;
 
 		this.torusPositionZ = -1;
+		this.whiteLightDistance = 1;
 		this.addShader();
 		this.addTorus();
-		this.addLight();
+		this.addLights();
 
 		this.count = 0;
-
+		this.t = 0;
 	}
 
 	loop() {
+		this.loopTexture();
+		this.loopLights();
+		this.app.update();
+	}
+
+	loopTexture() {
 		const ratio = 0.2;
 		this.texture.offset.x += 0.001 * ratio;
 		this.texture.offset.y += 0.01 * ratio;
-		//this.torus.rotation.y += 0.01 * ratio;
-		//this.torusBloom.rotation.y += 0.02 * ratio;
-		this.app.update();
+	}
+
+	loopLights() {
+		this.t += 0.02;
+		this.lightGroup.position.x = this.whiteLightDistance * Math.cos( this.t ) + 0;
+		this.lightGroup.position.z = this.whiteLightDistance * Math.sin( this.t ) - 0;
+		this.whiteLight.intensity = Math.cos( this.t ) / 5 + 1;
 	}
 
 	onTextureLoaded() {
@@ -57,7 +74,16 @@ class Experience {
 			}`;
 
 
-		this.shaderMaterial = new THREE.ShaderMaterial( {
+		this.shaderMaterialRed = new THREE.ShaderMaterial( {
+			uniforms: {},
+			vertexShader: BLOOM_VERTEX_SHADER,
+			fragmentShader: BLOOM_VERTEX_FRAGMENT,
+			side: THREE.FrontSide,
+			blending: THREE.AdditiveBlending,
+			transparent: true
+		} );
+
+		this.shaderMaterialWhite = new THREE.ShaderMaterial( {
 			uniforms: {},
 			vertexShader: BLOOM_VERTEX_SHADER,
 			fragmentShader: BLOOM_VERTEX_FRAGMENT,
@@ -69,9 +95,9 @@ class Experience {
 
 	addTorusBloom() {
 
-		const torusBloomGeometry = new THREE.TorusBufferGeometry( 0.39, 0.08, 16, 98 );
+		const torusBloomGeometry = new THREE.TorusBufferGeometry( 0.39, 0.08, 32, 64 );
 		torusBloomGeometry.center();
-		this.torusBloom = new THREE.Mesh( torusBloomGeometry, this.shaderMaterial );
+		this.torusBloom = new THREE.Mesh( torusBloomGeometry, this.shaderMaterialRed );
 		//this.torusBloom.scale.set( 0.90, 0.95, 2 );
 		this.torus.add( this.torusBloom );
 
@@ -105,13 +131,34 @@ class Experience {
 
 	}
 
-	addLight() {
+	addLights() {
 
+		// main light
 		this.light = new THREE.PointLight( 0xFF0000, 1, 1.25 );
 		this.light.position.set( 0, 1.2, this.torusPositionZ + 1 );
-		this.light.castShadow = true;
 		this.scene.add( this.light );
+
+		// moving white light
+		this.lightGroup = new THREE.Group();
+		this.torus.add( this.lightGroup );
+
+		const geometry = new THREE.SphereGeometry( 0.02, 32, 16 );
+		this.whiteLightSphere = new THREE.Mesh( geometry, this.shaderMaterialWhite );
+		this.lightGroup.add( this.whiteLightSphere );
+
+		this.whiteLight = new THREE.PointLight( new THREE.Color( 0x560000 ), 0.3, 3 );
+		this.lightGroup.add( this.whiteLight );
+
+		//const pointLightHelper = new THREE.PointLightHelper( this.whiteLight, 0.2 );
+		//this.lightGroup.add( pointLightHelper );
+		//this.lightGroup.position.y = -0.6;
+
+		const whiteLightFolder = this.gui.addFolder( 'whitelight' );
+		whiteLightFolder.add( this.whiteLight, 'intensity', 0, 20, 0.1 );
+		whiteLightFolder.addColor( this.whiteLight, 'color' );
+		whiteLightFolder.open();
+
 	}
 }
 
-
+Experience.prototype.gui = new GUI();
